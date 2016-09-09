39dbe2f1ee3372ee5b3f043717d72a5573f25204
xiaoshuang112 <warriordeng@163.com>
2016-09-09 19:41:24 +0800

    Add mouse event

---
+++ Opencv_skills.cpp
@@ -0,0 +1,51 @@
+void onMouse( int event, int x, int y, int, void*para )
+{   
+	CFDetectionDlg *Cdlg = (CFDetectionDlg *)para;
+	if(CV_EVENT_LBUTTONDOWN == event)
+	{
+	//	g_is_rect_inited = true;
+		g_rect_tl = cvPoint(x, y);
+		rect.x = x;
+		rect.y = y;
+		Cdlg->AddString(BLUE,"retc.x = %d,rect.y = %d",x, y);
+		
+	}
+
+	else if (CV_EVENT_LBUTTONUP == event )
+	{   
+		cvCopy(cvPhoto, copyPhoto);
+		g_rect_br = cvPoint(x, y);
+		cvRectangle(copyPhoto,g_rect_br,g_rect_tl, cvScalar(0,0,0), 5);
+		
+
+		cvShowImage(SrcDlg, copyPhoto);
+
+		rect.height = abs(g_rect_tl.y - g_rect_br.y);
+		rect.width  = abs(g_rect_tl.x - g_rect_br.x);
+		Cdlg->AddString(BLUE,"rect.height = %d,rect.width = %d",rect.height, rect.width);
+//复制ROI 并且释放
+		proPhoto = cvCreateImage(cvSize(rect.width,rect.height), 8, 3);
+		
+	 	cvSetImageROI(copyPhoto, rect); //set ROI, 
+
+		cvCopy(copyPhoto, proPhoto); 
+		cvResetImageROI(copyPhoto);
+		//cvReleaseImage(&copyPhoto);
+		//copyPhoto = NULL;
+
+		cvShowImage(DstDlg, proPhoto);
+
+		CTime time = ::GetCurrentTime();
+		CString RecordTime;
+		RecordTime.Format("%02d-%02d-%02d", time.GetHour(), time.GetMinute(), time.GetSecond());
+
+		CString path =GetModulePath() + "JT\\" + RecordTime + ".jpg";
+		cvSaveImage(path, proPhoto);
+
+		cvReleaseImage(&proPhoto);
+		proPhoto = NULL;
+	}
+}
+
+
+//调用方法：cvSetMouseCallback( SrcDlg, onMouse, this);
